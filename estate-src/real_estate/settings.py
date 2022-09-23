ba6d4f1cf785396fb4cd8f920a299e509cf90ba9
bazhil <i-bazh@mail.ru>
2022-09-24 01:32:43 +0300

    Refactor apps + add to settings

--- estate-src/real_estate/settings.py
+++ estate-src/real_estate/settings.py
@@ -30,14 +30,33 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites'
 ]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'django_filters',
+    'django_countries',
+    'phonenumber_field'
+]
+
+LOCAL_APPS = [
+    'apps.common',
+    'apps.users',
+    'apps.ratings',
+    'apps.profiles'
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
