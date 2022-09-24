e85c9993d17d232d4018a1aa930796ea5dc91da2
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2022-09-24 21:09:42 +0300

    add create_superuser

--- estate-src/apps/users/managers.py
+++ estate-src/apps/users/managers.py
@@ -41,3 +41,28 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, username, first_name, last_name, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superusers must have is_staff=True'))
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superusers must have is_superuser=True'))
+
+        if not password:
+            raise ValueError(_('Superusers must have a password'))
+
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_('Admin Account: An email address is required'))
+
+        user = self.create_user(username, first_name, last_name, email, password, **extra_fields)
+        user.save(using=self._db)
+
+        return user
